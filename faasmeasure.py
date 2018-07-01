c3a070cf63941f1b968e891744be8fbe0458382b
spio <spio@tiguriner>
2018-07-01 22:17:44 +0200

    - experimental/untested multi-threaded invocations

--- faasmeasure.py
+++ faasmeasure.py
@@ -1,14 +1,48 @@
-# run: python3 -u faasmeasure.py | tee faasmeasure.log
-
 import boto3
 import json
+import threading
+import random
+import os
+import sys
 
+numpar = 3
+simulate = True
+
+# FIXME
+full = True
 ccost = 0
-while True:
-    lambdainvocation = boto3.client("lambda")
-    fullresponse = lambdainvocation.invoke(FunctionName="proxycollectiveshipment", Payload="{}")
-    response = json.loads(fullresponse["Payload"].read().decode("utf-8"))
+
+def process():
+    global ccost, full
+
+    if simulate:
+        response = {"cost": 5.0, "remaining": random.randrange(9)}
+    else:
+        fullresponse = lambdainvocation.invoke(FunctionName="proxycollectiveshipment", Payload="{}")
+        response = json.loads(fullresponse["Payload"].read().decode("utf-8"))
     ccost += response["cost"]
     print("{:4d} remaining, cumulative cost {:.8} USD".format(response["remaining"], ccost))
     if response["remaining"] <= 0:
-        break
+        full = False
+
+def measure():
+    while full:
+        lambdainvocation = boto3.client("lambda")
+        threads = []
+        for i in range(numpar):
+            if numpar == 1:
+                process()
+            else:
+                t = threading.Thread(target=process)
+                threads.append(t)
+                t.start()
+            if not full:
+                break
+        for t in threads:
+            t.join()
+
+if __name__ == "__main__":
+    f = open("data/faasmeasure.{}.{}.log".format(numpar, simulate), "w")
+    sys.stdout = f
+    measure()
+    f.close()
